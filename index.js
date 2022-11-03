3a9f644b319e0040359da696f9cb813815b13fad
alexSalazar0206 <alexandrajosetina10@gmail.com>
2022-11-03 13:57:13 -0400

    tareafrontend

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const form = document.querySelector('#form');
 const emailInput = document.querySelector('#email-input');
 const passwordInput = document.querySelector('#password-input');
@@ -13,8 +15,8 @@ const validation = (regex, e, element) => {
        element.classList.remove('border-2', 'border-rose-500');
     }
     else {
-        element.classList.remove('border-2', 'border-rose-500');
-        element.classList.add('border-2', 'border-green-500');
+        element.classList.remove('border-2', 'border-green-500');
+        element.classList.add('border-2', 'border-rose-500');
     }
 };
 
@@ -28,16 +30,62 @@ passwordInput.addEventListener('input', e => {
 
 });
 
-matchInput.addEventListener('input', e => {
-    const isValid = e.target.value === passwordInput.value;
-    const information = e.target.parentElement.children[1];
- if (isValid) {
-    matchInput.classList.add('correct');
-    matchInput.classList.remove('incorrect');
-  }
-  else {
-    matchInput.classList.remove('correct');
-    matchInput.classList.add('incorrect');
-  }
+form.addEventListener('submit', async e => {
+    e.preventDefault();
+    try {
+        const newUser = {
+            email: emailInput.value,
+            password: passwordInput.value
+        }
+        const response = await fetch('http://localhost:3003/api/users', {
+            method: 'POST', 
+            headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser),
+
+        }); 
+    console.log(response.status);
+} catch (error) {
+console.log(error);
+}
+
+});
+
+
+// matchInput.addEventListener('input', e => {
+//     const isValid = e.target.value === passwordInput.value;
+//     const information = e.target.parentElement.children[1];
+//  if (isValid) {
+//     matchInput.classList.add('correct');
+//     matchInput.classList.remove('incorrect');
+//   }
+//   else {
+//     matchInput.classList.remove('correct');
+//     matchInput.classList.add('incorrect');
+//   }
  
- });
+//  });
+
+ 
+// countrySelect.addEventListener('input', e => {
+//     const selectedCountry = [...e.target.children].find(option => option.selected === true);
+//    phoneCode.innerHTML = `+${selectedCountry.value}`;
+   
+// });
+
+// const getCountry = async () => {
+//     const {country} = await (await fetch('https://api.geoapify.com/v1/ipinfo?&apiKey=07d9232cf7e4414385bc93ef1a521ee1', {method:'GET'})).json();
+//     [...countrySelect.children].forEach(option => {
+//     if (option.getAttribute('data-countryCode') === country.iso_code){
+//         option.selected = true;
+//         phoneCode.innerHTML = `+${option.value}`
+//         countrySelect.classList.add('correct');
+//         phoneCode.classList.add('correct');
+        
+
+
+//     }
+//   });
+// }
+
